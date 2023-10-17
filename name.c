01bb03276ceac2392968c099ce5f3a78ed8ba846
Gaurang Santoria <72335713+Gaurang2908@users.noreply.github.com>
2023-10-17 09:09:53 +0530

    Create name.c

---
+++ name.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+int main() 
+{ 
+	char name[20]; 
+	printf("Enter name: "); 
+
+	// user input will be taken here 
+	scanf("%s", name); 
+	printf("Your name is %s.", name); 
+	return 0; 
+}
